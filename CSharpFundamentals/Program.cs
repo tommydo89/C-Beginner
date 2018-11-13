b3646bd2af8ad796083b02c91b21b9c9d04f950d
Tommy Do <tommy.td89@gmail.com>
2018-11-12 21:52:18 -0500

    Add project files.

---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
